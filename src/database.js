ac6eae43e1f4dc04dd7b18216811804a8a208b78
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2022-09-30 20:40:02 -0500

    database config

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/notes-app-db')
+mongoose.connect(process.env.noteappdb || 'mongodb://localhost/notes-app-db')
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
